3d86e94d3d05838eab65fb63193ac923659cedd7
Vanessa Abreu <vanessa.lopes051@gmail.com>
2021-08-27 12:06:55 -0300

    Adiciando evento hide nas seções

---
+++ js/js.js
@@ -0,0 +1,24 @@
+
+
+var btn = document.getElementById('btnQ1a');
+var sectionQ1a = document.querySelector('.q1a');
+var sectionQ1b = document.querySelector('.q1b');
+
+
+btn.addEventListener('click', function() {
+  if(sectionQ1a.style.display === 'block') {
+      sectionQ1a.style.display = 'none';
+  } else {
+      sectionQ1a.style.display = 'block';
+  }
+});
+
+
+btnQ1b.addEventListener('click', function() {
+	console.log(btnQ1b)
+  if(sectionQ1b.style.display === 'block') {
+      sectionQ1b.style.display = 'none';
+  } else {
+      sectionQ1b.style.display = 'block';
+  }
+});
